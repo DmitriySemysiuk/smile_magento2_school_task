c26e085d7bd51b8889f3c839dac21030f53c17a2
dmitriysemysiuk <dmitriysemysiuk@gmail.com>
2019-08-05 22:55:24 +0300

    Add edit controller, layout, form ui_component, dataprovider and edit buttons: back, save, send-email"

---
+++ Customer/Block/Adminhtml/PriceRequest/Edit/SendEmailButton.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Smile\Customer\Block\Adminhtml\PriceRequest\Edit;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+/**
+ * Class SendEmailButton
+ *
+ * @package Smile\Customer\Block\Adminhtml\PriceRequest\Edit
+ */
+class SendEmailButton extends GenericButton implements ButtonProviderInterface
+{
+    const SEND_EMAIL_URL = 'smile_customer/pricerequest/sendemail';
+    /**
+     * Get SendEmailButton button data
+     *
+     * @return array
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Send email'),
+            'class' => 'primary',
+            'on_click' => sprintf("location.href = '%s';", $this->getSendEmailUrl()),
+            'sort_order' => 30,
+        ];
+    }
+
+    /**
+     * Get URL for send email button
+     *
+     * @return string
+     */
+    public function getBackUrl()
+    {
+        return $this->getUrl(self::SEND_EMAIL_URL);
+    }
+}
---
+++ Customer/Controller/Adminhtml/PriceRequest/Save.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Smile\Customer\Controller\Adminhtml\PriceRequest;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Smile\Customer\Api\PriceRequestRepositoryInterface;
+use Smile\Customer\Model\PriceRequestFactory;
+
+
+/**
+ * Class Save
+ *
+ * @package Smile\Customer\Controller\Adminhtml\PriceRequest
+ */
+class Save extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Smile_Customer::price_request_save';
+
+    /**
+     * Price Requests Closed Status
+     */
+    const STATUS_CLOSED = 3;
+
+    /**
+     * Data persistor interface
+     *
+     * @var DataPersistorInterface
+     */
+    private $dataPersistor;
+
+    /**
+     * Price request repository interface
+     *
+     * @var PriceRequestRepositoryInterface
+     */
+    private $priceRequestRepository;
+
+    /**
+     * Price request factory
+     *
+     * @var PriceRequestFactory
+     */
+    private $priceRequestFactory;
+
+    /**
+     * Save constructor
+     *
+     * @param Context          $context
+     * @param DataPersistorInterface  $dataPersistor
+     * @param PriceRequestRepositoryInterface $priceRequestRepository
+     * @param PriceRequestFactory             $priceRequestFactory
+     */
+    public function __construct(
+        Context $context,
+        DataPersistorInterface $dataPersistor,
+        PriceRequestRepositoryInterface $priceRequestRepository,
+        PriceRequestFactory $priceRequestFactory
+    ) {
+        $this->dataPersistor = $dataPersistor;
+        $this->priceRequestRepository = $priceRequestRepository;
+        $this->priceRequestFactory = $priceRequestFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Save action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     *
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    public function execute()
+    {
+        var_dump("Save");
+        exit();
+    }
+}
