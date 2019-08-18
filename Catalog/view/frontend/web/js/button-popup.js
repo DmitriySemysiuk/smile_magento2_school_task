a8db11b58485931a69623f740d7428c104c1e00c
dmitriysemysiuk <dmitriysemysiuk@gmail.com>
2019-08-18 12:27:36 +0300

    Add requirejs-config and fix button-popup

--- Catalog/view/frontend/web/js/button-popup.js
+++ Catalog/view/frontend/web/js/button-popup.js
@@ -1,4 +1,4 @@
-require(
+define(
     [
         'jquery',
         'Magento_Ui/js/modal/modal'
@@ -7,34 +7,45 @@ require(
         $,
         modal
     ) {
-        let form = $('#price-request-form');
-        form.mage('validation', {});
-        let options = {
-            type: 'popup',
-            responsive: true,
-            innerScroll: true,
-            buttons: [{
-                text: $.mage.__('Send'),
-                class: '',
-                click: function () {
-                    if(form.validation('isValid')){
-                        $.ajax({
-                            url: form.attr('action'),
-                            data: form.serialize(),
-                            type: 'POST',
-                            success: function() {
-                                form[0].reset();
-                                $('#price-request-popup-form').modal('closeModal');
-                            }
-                        });
-                    }
-                }
-            }]
-        };
+        return function () {
+            let container = $("#price-request-popup-form");
+            let form = $('#price-request-form');
 
-        let popup = modal(options, $('#price-request-popup-form'));
-        $("#price-request-button").on('click',function(){
-            $("#price-request-popup-form").modal("openModal");
-        });
+            form.mage('validation', {});
+
+            let options = {
+                type: 'popup',
+                responsive: true,
+                innerScroll: true,
+                buttons: [
+                    {
+                        text: $.mage.__('Send'),
+                        class: 'action-primary action-accept',
+                        click: function () {
+                            if (form.validation('isValid')) {
+                                $.ajax({
+                                    url: form.attr('action'),
+                                    data: form.serialize(),
+                                    type: 'POST',
+                                    success: function () {
+                                        form[0].reset();
+                                        this.closeModal();
+                                    }
+                                });
+                            }
+                        }
+                    }
+                    ]
+            };
+
+            let popup = modal(options, container);
+            $("#price-request-button").on('click', function () {
+                container.modal("openModal");
+            });
+
+            $(".action-close").on("click", function(){
+                form[0].reset();
+            });
+        }
     }
 );
